3e458dc8f4f4c942c06bb0c453e0b9b6a6f47c1a
Lakshmi <lakshmi.nishthala@gmail.com>
2017-12-11 10:38:32 -0800

    Trying to set up the login route

--- models/users.js
+++ models/users.js
@@ -6,11 +6,23 @@ const bcrypt = require('bcrypt');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const UserSchema = new Schema({
-  userName: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   passwordHash: { type: String, required: true },
   secrets: [{ type: Schema.Types.ObjectId, ref: 'Secret' }]
 });
+
+UserSchema.plugin(uniqueValidator); // attaching unique validator to user schema
+// 3
+UserSchema.virtual("password")
+  .set(function(value) {
+    this.passwordHash = bcrypt.hashSync(value, 8); // the 8 here is the "cost factor" - create a hash for password
+  });
+
+// compare the user password with password here
+UserSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.passwordHash);
+};
+
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
